ffc4e44b92c1f6ea7e792930d860cd399c878105
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2023-12-03 21:43:27 +0100

    fix: check if user wrote a review using user's id

--- controllers/reviews_controller.js
+++ controllers/reviews_controller.js
@@ -7,7 +7,7 @@ exports.addReview = async (req, res) => {
     if (!(await checkIfBookIdIsValid(req)) || !(await checkIfBookExists(req))) {
       return res.status(404).json({ message: "Book not found" });
     }
-    if (await checkIfUserAlreadyWroteReviewForThisBook(req.body.bookId)) {
+    if (await checkIfUserAlreadyWroteReviewForThisBook(req)) {
       return res
         .status(409)
         .json({ message: "This user already has a review for this book" });
@@ -27,8 +27,11 @@ const checkIfBookExists = async (req) => {
   return await bookModel.findById(req.body.bookId);
 };
 
-const checkIfUserAlreadyWroteReviewForThisBook = async (bookId) => {
-  return await reviewModel.findOne({ createdFor: bookId });
+const checkIfUserAlreadyWroteReviewForThisBook = async (req) => {
+  return await reviewModel.findOne({
+    createdFor: req.body.bookId,
+    createdBy: req.userId,
+  });
 };
 
 const saveReview = async (req, res) => {
